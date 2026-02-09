2aad94b1c2b243f3f3ea4a0f8de998e24e21a45c
Mario Reder <mario.reder@pm.me>
2026-02-09 14:27:14 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://sideswap.finance",
+  "VITE_SEO_SITE_URL": "https://ak47.ai",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
